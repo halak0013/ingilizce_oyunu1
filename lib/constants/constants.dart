3af74430894bd3f6eb3e93a392c6cf512189a636
mmtdemr42 <mmt.demr42@gmail.com>
2022-03-12 19:17:59 +0300

    ana ekran tasarımı yapılmaya başlandı.

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
-final Color COLOR_BLACK = Colors.black;
-final Color COLOR_MENU_BEYAZ = Color.fromARGB(255, 230, 213, 100);
-final Color COLOR_YAVYUAGZI = Color.fromARGB(255, 200, 166, 100);
-final Color COLOR_ARKAPLAN_BEYAZ = Color.fromARGB(255, 242, 234, 100);
+const Color COLOR_BLACK = Colors.black;
+const Color COLOR_MENU_BEYAZ = Color.fromARGB(255, 230, 213, 100);
+const Color COLOR_YAVYUAGZI = Color.fromARGB(255, 200, 166, 100);
+const Color COLOR_ARKAPLAN_BEYAZ = Color.fromARGB(255, 242, 234, 100);
+const Color COLOR_BASLIK_BORDO = Color.fromARGB(255, 112, 4, 4);
+
 
--- lib/screen/anasayfa.dart
+++ lib/screen/anasayfa.dart
@@ -1,13 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:ingilizce_oyunu1/widgetlar/anaEleman.dart';
+
+import '../constants/constants.dart';
 
 class AnaSayfa extends StatelessWidget {
-  const AnaSayfa({ Key? key }) : super(key: key);
+  AnaSayfa({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Faster Speaking"),
+          backgroundColor: COLOR_BASLIK_BORDO,
+        ),
+        body: Column(
+          children: [
+            AnaEleman(
+              child:
+                  ElevatedButton(child: Text("merhananan"), onPressed: (() {})),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
-
