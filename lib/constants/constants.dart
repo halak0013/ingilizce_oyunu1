7ea74a7b9a54440e71b7ec2ee07bce9a92d32e1e
Muhammet_Halak <halak@vuhuv.com>
2022-03-16 17:48:39 +0300

    renkler düzgün hale getirildi

--- lib/constants/constants.dart
+++ lib/constants/constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 const Color COLOR_BLACK = Colors.black;
-const Color COLOR_MENU_BEYAZ = Color.fromARGB(255, 255, 244, 173);
-const Color COLOR_YAVYUAGZI = Color.fromARGB(255, 255, 191, 88);
-const Color COLOR_ARKAPLAN_BEYAZ = Color.fromARGB(255, 242, 234, 100);
+const Color COLOR_MENU_BEYAZ = Color.fromARGB(255, 255, 242, 234);
+const Color COLOR_YAVYUAGZI = Color.fromARGB(255, 254, 205, 170);
+const Color COLOR_ORTA_BEYAZ = Color.fromARGB(255, 255, 230, 213);
 const Color COLOR_BASLIK_BORDO = Color.fromARGB(255, 88, 0, 0);
 
 const double BUTTON_GENISLIK=151;
