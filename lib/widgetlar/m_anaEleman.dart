65cdd5cdcac47383138d0398237d61175e244936
Muhammet_Halak <halak@vuhuv.com>
2022-03-16 08:17:23 +0300

    ana eleman2 sınıfı oluşturuldu

---
+++ lib/widgetlar/m_anaEleman.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AnaElaman2 extends StatelessWidget {
+  const AnaElaman2({ 
+    Key? key 
+  
+  
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+
+    );
+  }
+}
